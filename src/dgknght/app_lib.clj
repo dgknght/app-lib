5491eeb471df4ddc37ee1c7a093bc3a5cf4ca28b
Doug Knight <dougbknight@gmail.com>
2021-03-01 19:12:09 -0600

    create project from template

---
+++ src/dgknght/app_lib.clj
@@ -0,0 +1,6 @@
+(ns dgknght.app-lib)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/dgknght/app_lib_test.clj
@@ -0,0 +1,7 @@
+(ns dgknght.app-lib-test
+  (:require [clojure.test :refer :all]
+            [dgknght.app-lib :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
