3ed8a192af81189fa5c04ddc3db77aa68b15b7ff
Doug Knight <dougbknight@gmail.com>
2021-03-03 20:25:09 -0600

    add cljsbuild

--- project.clj
+++ project.clj
@@ -3,6 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.758"]]
   ; TODO: add resource-paths, in particular, reference the output of cljs build
-  :repl-options {:init-ns dgknght.app-lib})
+  :repl-options {:init-ns dgknght.app-lib}
+  :plugins [[lein-cljsbuild "1.1.8"]]
+  :cljsbuild {:builds [{:source-paths ["src/dgknght"]
+                        :compiler {:output-to "target/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}
+                        :jar true}]}
+  :hooks [leiningen.cljsbuild])
