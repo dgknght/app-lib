e0fa877484c9a8d3aeded11cacc35bf897235fa8
Doug Knight <dougbknight@gmail.com>
2023-02-17 14:52:23 -0600

    api tests

--- test/dgknght/app_lib/api_test.clj
+++ test/dgknght/app_lib/api_test.clj
@@ -1,6 +1,6 @@
 (ns dgknght.app-lib.api-test
   (:require [clojure.test :refer [deftest is #_testing]]
-            #_[clojure.tools.logging :as logging]
+            [clojure.tools.logging :as log]
             [dgknght.app-lib.test-assertions]
             [dgknght.app-lib.validation :as v]
             [dgknght.app-lib.api :as api]))
@@ -39,8 +39,44 @@
                                          ::v/errors ["age is required"]}))
       "An invalid model gets 400 status"))
 
-
 (deftest create-a-no-content-response
   (is (comparable? {:status 204
                     :body nil}
                    (api/response))))
+
+(deftest authenticate-a-request
+  (let [passed-req (atom nil)
+        handler (fn [req]
+                  (reset! passed-req req)
+                  {:status 200
+                   :body "OK"})
+        wrapped (api/wrap-authentication
+                  handler
+                  {:authenticate-fn (fn [req]
+                                      (when (= "Basic abc"
+                                               (get-in req [:headers "Authorization"]))
+                                        {:name "John Doe"}))})]
+    (let [res (wrapped {:headers {"Authorization" "Basic abc"}})]
+      (is (http-success? res)
+          "A request with valid Authorization succeeds")
+      (is (= {:name "John Doe"}
+             (:authenticated @passed-req))
+          "The authenticated entity is added to the request map"))
+    (is (http-unauthorized? (wrapped {}))
+        "A request without Authorization is unauthorized")
+    (is (http-unauthorized? (wrapped {:headers {"Authorization" "Basic def"}}))
+        "A request witn invalid Authorization is unauthorized")))
+
+(deftest handle-request-errrors
+  (let [logged (atom [])
+        handler (fn [_req]
+                  (throw (ex-info "induced error" {:my :data})))
+        wrapped (api/wrap-api-exception handler)
+        res (with-redefs [log/log* (fn [& args]
+                                     (swap! logged conj args))]
+              (wrapped {}))]
+    (is (= 500 (:status res))
+        "The response indicates an internal error")
+    (is (= :error
+           (second (first @logged)))
+        "The error details are logged")))
