191da6d92519868807e874ce9bc74a203c090f7b
Doug Knight <dougbknight@gmail.com>
2026-07-19 20:11:12 -0500

    add fraction-digits to decimal-input

--- test/dgknght/app_lib/forms_test.cljs
+++ test/dgknght/app_lib/forms_test.cljs
@@ -1,5 +1,5 @@
 (ns dgknght.app-lib.forms-test
-  (:require [cljs.test :refer-macros [deftest is]]
+  (:require [cljs.test :refer-macros [deftest testing is]]
             [reagent.core :as r]
             [dgknght.app-lib.forms :as forms]))
 
@@ -11,3 +11,21 @@
     (is (= :input tag))
     (is (= 0 min-attr))
     (is (= 100 max-attr))))
+
+(deftest decimal-input-arithmatic
+  (testing "default behavior"
+    (let [model (r/atom {:amount nil})
+          field [:amount]
+          render-fn (forms/decimal-input model field {})
+          [_tag {on-change :on-change}] (render-fn)]
+      (on-change #js {:target #js {:value "9.99 * 1.0825"}})
+      (is (= "10.814175" (str (get-in @model field)))
+          "The value is not rounded.")))
+  (testing "with :fraction-digits 2"
+    (let [model (r/atom {:amount nil})
+          field [:amount]
+          render-fn (forms/decimal-input model field {:fraction-digits 2})
+          [_tag {on-change :on-change}] (render-fn)]
+      (on-change #js {:target #js {:value "9.99 * 1.0825"}})
+      (is (= "10.81" (str (get-in @model field)))
+          "The value is rounded to 2 digits."))))
